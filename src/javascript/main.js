3f3ff7597496b2efe8e1798ed1b903ba5d6b2a07
Zidan Herlangga <133259343+zidanherlangga04@users.noreply.github.com>
2023-06-16 15:11:24 +0700

    Add files via upload

---
+++ src/javascript/main.js
@@ -0,0 +1,37 @@
+// function halo() {
+//   console.log("heloo");
+// }
+
+document.querySelectorAll(".btnDetail").forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let parent = e.target.parentNode.parentNode;
+
+    let gambar = parent.querySelector(".card-img-top").src;
+    let harga = parent.querySelector(".harga").innerHTML;
+    let judul = parent.querySelector(".card-text").innerHTML;
+    let deskripsi = parent.querySelector(".deskripsi")
+      ? parent.querySelector(".deskripsi").innerHTML
+      : "<i>Tidak ada informasi yang tersedia</i>";
+
+    // Modal
+    let tombolModal = document.querySelector(".btnModal");
+    tombolModal.click();
+
+    document.querySelector(".modalTitle").innerHTML = judul;
+
+    let image = document.createElement("img");
+    image.src = gambar;
+    image.classList.add("w-100");
+
+    document.querySelector(".modalImage").innerHTML = "";
+    document.querySelector(".modalImage").appendChild(image);
+    document.querySelector(".modalDeskripsi").innerHTML = deskripsi;
+    document.querySelector(".modalHarga").innerHTML = harga;
+
+    // Nomer HP
+    const noHP = "6285157066514";
+    let pesan = `https://api.whatsapp.com/send?phone=${noHP}&text=Halo Bang, saya mau beli produk ini ${gambar}`;
+
+    document.querySelector(".btnBeli").href = pesan;
+  });
+});
