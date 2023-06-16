442e3d855b4e5035706e76eab1f1dd8d37680256
Zidan Herlangga <133259343+zidanherlangga04@users.noreply.github.com>
2023-06-16 18:36:33 +0700

    Add files via upload

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,7 +1,4 @@
-// function halo() {
-//   console.log("heloo");
-// }
-
+// Fungsi ketika tombol detail di klik
 document.querySelectorAll(".btnDetail").forEach((item) => {
   item.addEventListener("click", (e) => {
     let parent = e.target.parentNode.parentNode;
@@ -35,3 +32,25 @@ document.querySelectorAll(".btnDetail").forEach((item) => {
     document.querySelector(".btnBeli").href = pesan;
   });
 });
+
+// Botton Top Home
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
